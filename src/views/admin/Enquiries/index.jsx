7cfc89729ef87316b94e4bde28f0c3bd8cfb9c9f
EseAlli <essealli.ea@gmail.com>
2023-09-10 13:11:06 +0100

    enquires page quick fix and test

--- src/views/admin/Enquiries/index.jsx
+++ src/views/admin/Enquiries/index.jsx
@@ -19,62 +19,59 @@ const Enquiries = () => {
 
 	return (
 		<>
-			<div className="flex flex-w w-full self-start z-40">
-				{isLoading ? (
-					<Loader />
-				) : (
-					<div className="w-full px-4">
-						<Table width="full">
-							<TableHeaderRow className="grid grid-cols-[150px_150px_1fr_100px] gap-2">
-								{header.map(({ label }, index) => {
-									return <TableHeader key={index}>{label}</TableHeader>;
-								})}
-								<TableHeader></TableHeader>
-							</TableHeaderRow>
-							<TableBody loading={isLoading}>
+			<div className="w-full z-40 bg-white rounded-md">
+				<div className="flex items-center justify-between px-4 mt-3">
+					<h5 className="font-medium text-xl">Enquiries</h5>
+				</div>
+				<Table width="full">
+					<TableHeaderRow className="grid grid-cols-[150px_150px_1fr_100px] gap-x-4">
+						{header.map(({ label }, index) => {
+							return <TableHeader key={index}>{label}</TableHeader>;
+						})}
+						<TableHeader></TableHeader>
+					</TableHeaderRow>
+					<TableBody loading={isLoading}>
+						<>
+							{isLoading ? (
+								<div className="min-h-[200px] flex items-center">
+									<BarrLoader />
+								</div>
+							) : (
 								<>
-									{isLoading ? (
-										<div className="min-h-[200px] flex items-center">
-											<BarrLoader />
-										</div>
-									) : (
-										<>
-											{data.map(
-												(
-													{
-														_id,
-														fullName,
-														email,
-														description,
+									{data.map(
+										(
+											{
+												_id,
+												fullName,
+												email,
+												description,
 
-														createdAt,
-													},
-													index
-												) => {
-													return (
-														<TableDataRow
-															key={index}
-															className="grid grid-cols-[150px_150px_1fr_100px] px-4 py-3 gap-2 bg-white">
-															<TableData>
-																<span>{fullName}</span>
-															</TableData>
-															<TableData>{email}</TableData>
-															<TableData noTruncate>{description}</TableData>
+												createdAt,
+											},
+											index
+										) => {
+											return (
+												<TableDataRow
+													key={index}
+													className="grid grid-cols-[150px_150px_1fr_100px] px-4 py-3 gap-x-4 bg-white">
+													<TableData>
+														<span>{fullName}</span>
+													</TableData>
+													<TableData>{email}</TableData>
+													<TableData noTruncate>{description}</TableData>
 
-															<TableData>
-																{moment(createdAt).format("DD MMM, YYYY")}
-															</TableData>
-														</TableDataRow>
-													);
-												}
-											)}
-										</>
+													<TableData>
+														{moment(createdAt).format("DD MMM, YYYY")}
+													</TableData>
+												</TableDataRow>
+											);
+										}
 									)}
 								</>
-							</TableBody>
-						</Table>
-					</div>
-				)}
+							)}
+						</>
+					</TableBody>
+				</Table>
 			</div>
 		</>
 	);
