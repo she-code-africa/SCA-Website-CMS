40fa59c51529c7002ead225f3ffeeac80fea19ac
chidaluanukam@gmail.com <chidalu.anukam@charisol.io>
2025-04-21 09:29:22 +0100

    Revert "fix: add pagination to chapters"
    This reverts commit dab0659a4829b0dbad21eef14ad60cbfab195191.

--- src/views/admin/Chapters/index.jsx
+++ src/views/admin/Chapters/index.jsx
@@ -23,7 +23,6 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ChapterModal from "components/Chapters/ChapterModal";
 import Category from "components/Categories";
-import ChapterPagination from "components/Chapters/ChapterPagination";
 
 const Chapters = () => {
 	const [selectedId, setSelectedId] = useState();
@@ -34,9 +33,6 @@ const Chapters = () => {
 	const [chapters, setChapters] = useState([]);
 	const [categories, setCatgeories] = useState([]);
 	const [categoryId, setCategoryId] = useState();
-	const [page, setPage] = useState(1);
-	const [totalPages, setTotalPages] = useState(0);
-	const [currentPage, setCurrentPage] = useState(1);
 
 	useQuery("chapter-categories", getChapterCategories, {
 		onSuccess: (data) => {
@@ -61,11 +57,9 @@ const Chapters = () => {
 		},
 	});
 
-	const { isLoading } = useQuery(["chapters", page], () => getChapters(page), {
+	const { isLoading } = useQuery("chapters", getChapters, {
 		onSuccess: (data) => {
-			setChapters(data.data);
-			setTotalPages(data.totalPages);
-			setCurrentPage(data.currentPage);
+			setChapters(data);
 		},
 		onError: (err) => {
 			toast.error("Could not fetch Chapters");
@@ -94,10 +88,6 @@ const Chapters = () => {
 
 	const handleDelete = () => {
 		deleteMember({ id: selectedId, categoryId: categoryId });
-	};
-
-	const handlePageChange = (page) => {
-		setPage(page);
 	};
 
 	return (
@@ -187,13 +177,6 @@ const Chapters = () => {
 						remove={removeCategory}
 						update={updateCategory}
 					/>
-					<div className="">
-						<ChapterPagination
-							onPageChange={handlePageChange}
-							totalPages={totalPages}
-							currentPage={currentPage}
-						/>
-					</div>
 				</div>
 			</div>
 			{isDeleteModalOpen && (
