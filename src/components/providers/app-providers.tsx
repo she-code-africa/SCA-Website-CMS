7e5dc5c8a1dfde6b911d5ea876d6ea2b919f22fa
glowwEE <gloryokafor5@gmail.com>
2026-03-14 09:27:34 +0000

    renamed global error

--- src/components/providers/app-providers.tsx
+++ src/components/providers/app-providers.tsx
@@ -41,37 +41,29 @@
 //   );
 // }
 
-// "use client";
-
-// import * as React from "react";
-// import { GoogleOAuthProvider } from "@react-oauth/google";
-// import { ThemeProvider } from "@/components/providers/theme-provider";
-// import { QueryProvider } from "@/components/providers/react-query-provider";
-// import { Toaster } from "@/components/ui/sonner";
-
-// export function AppProviders({ children }: { children: React.ReactNode }) {
-//   const clientId = process.env.NEXT_PUBLIC_GA_CLIENT_ID;
-
-//   const inner = (
-//     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-//       <QueryProvider>
-//         {children}
-//         <Toaster richColors position="top-right" />
-//       </QueryProvider>
-//     </ThemeProvider>
-//   );
-
-//   if (!clientId) {
-//     return inner;
-//   }
-
-//   return <GoogleOAuthProvider clientId={clientId}>{inner}</GoogleOAuthProvider>;
-// }
-
 "use client";
 
 import * as React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { QueryProvider } from "@/components/providers/react-query-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export function AppProviders({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  const clientId = process.env.NEXT_PUBLIC_GA_CLIENT_ID;
+
+  const inner = (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <QueryProvider>
+        {children}
+        <Toaster richColors position="top-right" />
+      </QueryProvider>
+    </ThemeProvider>
+  );
+
+  if (!clientId) {
+    return inner;
+  }
+
+  return <GoogleOAuthProvider clientId={clientId}>{inner}</GoogleOAuthProvider>;
 }
